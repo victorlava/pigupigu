e6af6e9a92950f4dbbfd1102eb79efcc01897ddc
vagrant <vagrant@homestead>
2019-05-04 19:34:05 +0000

    added tables

---
+++ database/migrations/2019_05_04_192911_create_prices_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePricesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prices', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->float('price');
+            $table->unsignedBigInteger('product_id');
+            $table->timestamps();
+
+            $table->foreign('product_id')->references('id')->on('products');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prices');
+    }
+}
